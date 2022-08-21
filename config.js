ef84eccc66fbbfaced2d3f76e29f3f56913ca9fd
offcind <110681096+offcind@users.noreply.github.com>
2022-08-21 14:29:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.owner = ['6285717281107']
+global.premium = ['6285717281107']
+global.packname = 'Class-71'
+global.author = '71-MD'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
